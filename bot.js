d2e1b8c5a6d8358a8e1d73b16a6f23091284ae56
B̴l̷i̷t̶x̷ <59416533+Blitzcryp@users.noreply.github.com>
2022-01-23 00:11:27 +0200

    Deploy FIrst Feature

--- bot.js
+++ bot.js
@@ -1,26 +1,76 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const replies = [
-    "my mom is gay",
-    "your mom is gay",
-    "why are our moms fucking"
-]
+require("dotenv").config();
 
-client.login(process.env.TOKEN);
+const baseCourseUrl = "https://curs.upb.ro/2021/course/view.php"
+const courses = {   
+    "OMII" : [8851, 3709],
+    "TCD": [8636, 3730],
+    "AAHP": [3741],
+    "AUT": [3737, 8848],
+    "IAC": [3703, 9817],
+    "MF": [3680, 10716],
+    "MEF": [3697, 8852],
+    "PPI": [3717],
+    "TMT": [3733, 10698],
+    "AMF": [3797],
+    "CAC": [3770],
+    "MI": [3773],
+    "MCE": [3805],
+    "MLCN": [3771],
+    "MecA": [3851],
+    "OG": [3769],
+    "Pratica": [3831],
+    "PPII": [3825],
+    "PIIP": [3850],
+    "VM": [3801],
+}
+
+client.login(process.env.BOT_TOKEN);
 
 client.on('ready', readyDiscord);
 client.on('message', gotMessage);
 
 function gotMessage(message){
-    if(message.channel.id ==="934549787787821066" && message.content === "choo choo"){
+    if(message.channel.id ===process.env.CHANNEL_ID && message.content === "!curs"){
         // message.reply("Train Rainbow Gay");
-        const index = Math.floor(Math.random() * replies.length);
+        let msg = "";
+        let index = 1;
+        Object.keys(courses).map((course) =>{
+            msg += index + ". " + course + "\n"; 
+            index += 1;
 
-        message.channel.send(replies[index]);
+            console.log(course);
+        })
+
+        message.channel.send(msg);
+    }
+
+    if(message.channel.id === process.env.CHANNEL_ID && message.content.includes("!curs-")){
+        msg = message.content.slice(6)
+
+        console.log(courses["ABC"]);
+
+        if(courses[msg] === undefined){
+            message.reply("I could not find this course. Try running '!curs' for a list of courses.");
+        } 
+
+        if(courses[msg]){
+            let index = 1;
+            let coursesLinks = "";
+            
+            for (const id of courses[msg]){
+                coursesLinks += index + ". " + baseCourseUrl + "?id=" + id + "\n";
+                index++;
+            }
+
+            message.reply("Check out: \n" + coursesLinks);
+        }
+
     }
 }
 
 function readyDiscord(){
-    console.log('<3');
+    console.log('❤️ u mada');
 }
